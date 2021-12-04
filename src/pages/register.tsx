41104f28c2f289b63646406335105d7bae071b01
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2021-12-05 05:22:34 +0600

    vanilla mutation request with urql

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -3,16 +3,32 @@ import { Form, Formik } from 'formik';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
 import { Box, Button } from '@chakra-ui/react';
+import { useMutation } from 'urql';
 
 interface registerProps {}
 
+const REGISTER_MUT = `mutation Register($username: String!, $password: String!) {
+  register(options: { username: $username, password: $password }) {
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      username
+    }
+  }
+}`;
+
 const Register: React.FC<registerProps> = ({}) => {
+  const [, register] = useMutation(REGISTER_MUT);
   return (
     <Wrapper>
       <Formik
         initialValues={{ username: '', password: '' }}
         onSubmit={(values) => {
           console.log(values);
+          return register(values);
         }}
       >
         {({ isSubmitting }) => {
