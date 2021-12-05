4667da4ab8ec07560c585eca864ea678bf20d87b
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2021-12-05 22:32:55 +0600

    using GraphQL code generated urql hook for register mutation

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -3,32 +3,19 @@ import { Form, Formik } from 'formik';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
 import { Box, Button } from '@chakra-ui/react';
-import { useMutation } from 'urql';
+import { useRegisterMutation } from '../generated/graphql';
 
 interface registerProps {}
 
-const REGISTER_MUT = `mutation Register($username: String!, $password: String!) {
-  register(options: { username: $username, password: $password }) {
-    errors {
-      field
-      message
-    }
-    user {
-      id
-      username
-    }
-  }
-}`;
-
 const Register: React.FC<registerProps> = ({}) => {
-  const [, register] = useMutation(REGISTER_MUT);
+  const [, register] = useRegisterMutation();
   return (
     <Wrapper>
       <Formik
         initialValues={{ username: '', password: '' }}
-        onSubmit={(values) => {
+        onSubmit={async (values) => {
           console.log(values);
-          return register(values);
+          const response = await register(values);
         }}
       >
         {({ isSubmitting }) => {
