59941771059ec7ec45cadc0110b99dadff61f423
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2021-12-07 04:35:42 +0600

    urql graphcache for updating "Me" cache data when login register mutation cache is saving

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,59 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
-import { Provider, createClient } from 'urql';
-
+import { createClient, Provider, dedupExchange, fetchExchange } from 'urql';
+import { cacheExchange, QueryInput } from '@urql/exchange-graphcache';
 import theme from '../theme';
+import {
+  LoginMutation,
+  MeDocument,
+  MeQuery,
+  RegisterMutation,
+} from '../generated/graphql';
 
 const client = createClient({
   url: 'http://localhost:4000/graphql',
   fetchOptions: { credentials: 'include' }, //have some cookie please
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        // updating query after our mutation fires
+        Mutation: {
+          login: (result: LoginMutation, _args, cache, _info) => {
+            cache.updateQuery<MeQuery, QueryInput>(
+              { query: MeDocument },
+              (data) => {
+                // you can do immer like updates
+                if (result.login.errors) {
+                  return data; // same as the cache
+                } else {
+                  return {
+                    me: result.login.user, // login success then, update Me cache with the user
+                  };
+                }
+              }
+            );
+          },
+          register: (result: RegisterMutation, _args, cache, _info) => {
+            cache.updateQuery<MeQuery, QueryInput>(
+              { query: MeDocument },
+              (data) => {
+                // you can do immer like updates
+                if (result.register.errors) {
+                  return data;
+                } else {
+                  return {
+                    me: result.register.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
