b85f84868a11aa404c674b5ce380ea262aa088ef
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2021-12-06 07:35:59 +0600

    login page with formik and urql ready to send a graphql request and receive cookies & data

---
+++ src/pages/login.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Box } from '@chakra-ui/layout';
+import { Button } from '@chakra-ui/react';
+import { Formik, Form } from 'formik';
+import { InputField } from '../components/InputField';
+import { Wrapper } from '../components/Wrapper';
+import { useLoginMutation } from '../generated/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
+import { useRouter } from 'next/router';
+
+interface LoginProps {}
+
+const Login: React.FC<LoginProps> = ({}) => {
+  const [, login] = useLoginMutation();
+  const router = useRouter();
+  return (
+    <Formik
+      initialValues={{ username: '', password: '' }}
+      onSubmit={async (values, { setErrors }) => {
+        const response = await login({ options: values });
+        if (response.data?.login.errors) {
+          setErrors(toErrorMap(response.data.login.errors));
+        } else if (response.data?.login.user) {
+          //worked
+          console.log(response.data.login.user);
+          router.push('/');
+        }
+      }}
+    >
+      {({ isSubmitting }) => {
+        return (
+          <Form>
+            <Wrapper>
+              <InputField
+                name="username"
+                type="text"
+                label="Username"
+                placeholder="Username"
+              />
+              <Box mt={4}>
+                <InputField
+                  name="password"
+                  type="password"
+                  label="Password"
+                  placeholder="Password"
+                />
+              </Box>
+              <Button
+                colorScheme="teal"
+                type="submit"
+                isLoading={isSubmitting}
+                mt={4}
+              >
+                Login
+              </Button>
+            </Wrapper>
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+};
+
+export default Login;
