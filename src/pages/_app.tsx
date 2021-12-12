e7675bf3294de513d0f9262e0f53eb5207327a0f
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2021-12-12 20:53:48 +0600

    removing the provider from _app and making a standalone config file(function) for urql (will pass to make a HOC as needed for Next.js)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,83 +1,19 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
-import { createClient, Provider, dedupExchange, fetchExchange } from 'urql';
-import { cacheExchange, QueryInput } from '@urql/exchange-graphcache';
-import theme from '../theme';
-import {
-  LoginMutation,
-  LogoutMutation,
-  MeDocument,
-  MeQuery,
-  RegisterMutation,
-} from '../generated/graphql';
 
-const client = createClient({
-  url: 'http://localhost:4000/graphql',
-  fetchOptions: { credentials: 'include' }, //have some cookie please
-  exchanges: [
-    dedupExchange,
-    cacheExchange({
-      updates: {
-        // updating query after our mutation fires
-        Mutation: {
-          logout: (_result: LogoutMutation, _args, cache, _info) => {
-            cache.updateQuery<MeQuery, QueryInput>(
-              { query: MeDocument },
-              () => {
-                return { me: null };
-              }
-            );
-          },
-          login: (result: LoginMutation, _args, cache, _info) => {
-            cache.updateQuery<MeQuery, QueryInput>(
-              { query: MeDocument },
-              (data) => {
-                // you can do immer like updates
-                if (result.login.errors) {
-                  return data; // same as the cache
-                } else {
-                  return {
-                    me: result.login.user, // login success then, update Me cache with the user
-                  };
-                }
-              }
-            );
-          },
-          register: (result: RegisterMutation, _args, cache, _info) => {
-            cache.updateQuery<MeQuery, QueryInput>(
-              { query: MeDocument },
-              (data) => {
-                // you can do immer like updates
-                if (result.register.errors) {
-                  return data;
-                } else {
-                  return {
-                    me: result.register.user,
-                  };
-                }
-              }
-            );
-          },
-        },
-      },
-    }),
-    fetchExchange,
-  ],
-});
+import theme from '../theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider value={client}>
-      <ChakraProvider resetCSS theme={theme}>
-        <ColorModeProvider
-          options={{
-            useSystemColorMode: true,
-          }}
-        >
-          <Component {...pageProps} />
-        </ColorModeProvider>
-      </ChakraProvider>
-    </Provider>
+    <ChakraProvider resetCSS theme={theme}>
+      <ColorModeProvider
+        options={{
+          useSystemColorMode: true,
+        }}
+      >
+        <Component {...pageProps} />
+      </ColorModeProvider>
+    </ChakraProvider>
   );
 }
 
---
+++ src/utils/createUrqlClient.ts
@@ -0,0 +1,64 @@
+import { dedupExchange, fetchExchange } from 'urql';
+import { cacheExchange, QueryInput } from '@urql/exchange-graphcache';
+import {
+  LoginMutation,
+  LogoutMutation,
+  MeDocument,
+  MeQuery,
+  RegisterMutation,
+} from '../generated/graphql';
+
+export const createUrqlClient = (ssrExchange: any) => ({
+  url: 'http://localhost:4000/graphql',
+  fetchOptions: { credentials: 'include' as const }, //have some cookie please
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        // updating query after our mutation fires
+        Mutation: {
+          logout: (_result: LogoutMutation, _args, cache, _info) => {
+            cache.updateQuery<MeQuery, QueryInput>(
+              { query: MeDocument },
+              () => {
+                return { me: null };
+              }
+            );
+          },
+          login: (result: LoginMutation, _args, cache, _info) => {
+            cache.updateQuery<MeQuery, QueryInput>(
+              { query: MeDocument },
+              (data) => {
+                // you can do immer like updates
+                if (result.login.errors) {
+                  return data; // same as the cache
+                } else {
+                  return {
+                    me: result.login.user, // login success then, update Me cache with the user
+                  };
+                }
+              }
+            );
+          },
+          register: (result: RegisterMutation, _args, cache, _info) => {
+            cache.updateQuery<MeQuery, QueryInput>(
+              { query: MeDocument },
+              (data) => {
+                // you can do immer like updates
+                if (result.register.errors) {
+                  return data;
+                } else {
+                  return {
+                    me: result.register.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    ssrExchange,
+    fetchExchange,
+  ],
+});
