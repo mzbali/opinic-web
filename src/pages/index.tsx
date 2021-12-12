2c982a3a7af50f3239b48b602460a2d01b0e6cb6
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2021-12-12 20:57:03 +0600

    passing urql client as it's needed to make any graphql query (also SSR)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { NavBar } from '../components/NavBar';
+import { withUrqlClient } from 'next-urql';
+import { createUrqlClient } from '../utils/createUrqlClient';
+import { usePostsQuery } from '../generated/graphql';
 
-const Index = () => (
-  <>
-    <NavBar />
-    <div>hello world</div>
-  </>
-);
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+  return (
+    <>
+      <NavBar />
+      <div>hello world</div>
+      <br />
+      {!data?.posts ? null : data.posts.map((post) => <p>{post.title}</p>)}
+    </>
+  );
+};
 
-export default Index;
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index); // withUrqlClient is a higher order components
