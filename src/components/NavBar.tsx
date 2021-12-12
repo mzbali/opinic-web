4ab5d9dbc043d06da54fef4242d99c2e084867ed
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2021-12-12 21:03:03 +0600

    not making unnecessary request, don't check 'Me' query if in Next.js server (since no cookie there), will check after hydration

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,14 @@ import { Box, Button, Flex, Link } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import React from 'react';
 import { useLogoutMutation, useMeQuery } from '../generated/graphql';
+import { isServer } from '../utils/isServer';
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
-  const [{ data, fetching }] = useMeQuery();
+  const [{ data, fetching }] = useMeQuery({
+    pause: isServer(), // when SSR, So in nextjs server, do not call the query, Since no cookie in next server
+  });
   const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
   let body = null;
   if (fetching) {
