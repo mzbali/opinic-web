f899c065b4935551136ad891a75ef03a85f2b8e3
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2022-02-03 03:13:11 +0600

    when logged in user will be taken back to on the page that user wanted to access before

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -24,8 +24,11 @@ const Login: React.FC<LoginProps> = ({}) => {
           setErrors(toErrorMap(response.data.login.errors));
         } else if (response.data?.login.user) {
           //worked
-          console.log(response.data.login.user);
-          router.push('/');
+          if (typeof router.query.next === 'string') {
+            router.push(router.query.next);
+          } else {
+            router.push('/');
+          }
         }
       }}
     >
