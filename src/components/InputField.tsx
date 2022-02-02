896ca0152f04dd89ef58ef8c445bb46c36622a6b
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2022-02-03 02:30:54 +0600

    an option to make input to textarea

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -5,6 +5,7 @@ import {
   FormLabel,
   Input,
   FormErrorMessage,
+  Textarea,
 } from '@chakra-ui/react';
 
 /* InputHTMLAttributes<HTMLInputElement> means take/expect everyting a regular input will take */
@@ -13,18 +14,24 @@ import {
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   name: string;
   label: string;
+  textarea?: boolean;
 };
 
 export const InputField: React.FC<InputFieldProps> = ({
   label,
   size: _,
+  textarea,
   ...props
 }) => {
+  let InputOrTextarea: any = Input;
+  if (textarea) {
+    InputOrTextarea = Textarea;
+  }
   const [field, { error, touched }] = useField(props);
   return (
     <FormControl isInvalid={!!error && touched}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Input {...field} {...props} id={field.name} />
+      <InputOrTextarea {...field} {...props} id={field.name} />
       {!!error && touched && <FormErrorMessage>{error}</FormErrorMessage>}
     </FormControl>
   );
