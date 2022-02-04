d025422fbf58dbc8a330f034e0e8c8a91fb79157
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2022-02-04 15:49:48 +0600

    pagination support for root page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withUrqlClient } from 'next-urql';
 import { createUrqlClient } from '../utils/createUrqlClient';
 import { usePostsQuery } from '../generated/graphql';
 import { Layout } from '../components/Layout';
-import { Box, Flex, Heading, Link, Stack, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Link,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
 import NextLink from 'next/link';
 
 const Index = () => {
-  const [{ data }] = usePostsQuery({ variables: { limit: 10, cursor: null } });
+  const [variables, setVariables] = useState({
+    limit: 10,
+    cursor: null as string | null,
+  });
+  const [{ data, fetching }] = usePostsQuery({
+    variables,
+  });
+  if (!fetching && !data) {
+    return <Text>There is no data for some reason</Text>;
+  }
   return (
     <Layout>
       <Flex align="center" mb={8}>
@@ -17,9 +34,9 @@ const Index = () => {
         </NextLink>
       </Flex>
       <Stack spacing={6}>
-        {!data?.posts
+        {fetching && !data?.posts
           ? 'loading...'
-          : data.posts.map((post) => {
+          : data!.posts.map((post) => {
               return (
                 <Box p={5} shadow="md" borderWidth="1px" key={post.id}>
                   <Heading fontSize="xl">{post.title}</Heading>
@@ -28,6 +45,21 @@ const Index = () => {
               );
             })}
       </Stack>
+      {data ? (
+        <Flex my={8} justify="center">
+          <Button
+            colorScheme="teal"
+            onClick={() =>
+              setVariables((state) => ({
+                limit: state.limit,
+                cursor: data.posts[data.posts.length - 1].createdAt,
+              }))
+            }
+          >
+            Load More...
+          </Button>
+        </Flex>
+      ) : null}
     </Layout>
   );
 };
