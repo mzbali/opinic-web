05a1ff47aec392fc5dcb49c8392635858fd51c75
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2022-02-10 22:10:33 +0600

    cache update with writeFragment when updoots and sending cookie to next server-> graphql endpoint

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -4,6 +4,7 @@ import {
   fetchExchange,
   stringifyVariables,
 } from 'urql';
+import { gql } from '@urql/core';
 import { cacheExchange, QueryInput, Resolver } from '@urql/exchange-graphcache';
 import { pipe, tap } from 'wonka';
 import {
@@ -12,8 +13,11 @@ import {
   MeDocument,
   MeQuery,
   RegisterMutation,
+  VoteMutation,
+  VoteMutationVariables,
 } from '../generated/graphql';
 import Router from 'next/router';
+import { isServer } from './isServer';
 
 export const errorExchange: Exchange =
   ({ forward }) =>
@@ -47,8 +51,9 @@ const cursorPagination = (): Resolver => {
     if (size === 0) {
       return undefined;
     }
-    const fieldkey = `${fieldName}${stringifyVariables(fieldArgs)}`;
-    const isItInTheCache = cache.resolve(entityKey, fieldkey);
+    const fieldKey = `${fieldName}${stringifyVariables(fieldArgs)}`;
+    const isItInTheCache = cache.resolve(entityKey, fieldKey);
+    console.log(isItInTheCache);
     info.partial = !isItInTheCache; // if in cache sill request again
     const result: string[] = [];
     let hasMore = true;
@@ -57,7 +62,7 @@ const cursorPagination = (): Resolver => {
       const data = cache.resolve(key, 'posts') as string[]; //since selection set
       const _hasMore = cache.resolve(key, 'hasMore');
       if (!_hasMore) {
-        hasMore = false;
+        hasMore = _hasMore as boolean;
       }
       result.push(...data); // every data from request 0 to n should be in the array, load more grow more
     });
@@ -69,66 +74,115 @@ const cursorPagination = (): Resolver => {
   };
 };
 
-export const createUrqlClient = (ssrExchange: any) => ({
-  url: 'http://localhost:4000/graphql',
-  fetchOptions: { credentials: 'include' as const }, //have some cookie please
-  exchanges: [
-    dedupExchange,
-    cacheExchange({
-      keys: {
-        PaginatedPosts: () => null,
-      },
-      resolvers: {
-        Query: {
-          posts: cursorPagination(),
+export const createUrqlClient = (ssrExchange: any, ctx: any) => {
+  let cookie = '';
+  if (isServer()) {
+    cookie = ctx.req.headers.cookie;
+  }
+
+  return {
+    url: 'http://localhost:4000/graphql',
+    fetchOptions: {
+      credentials: 'include' as const,
+      headers: cookie
+        ? {
+            cookie,
+          }
+        : undefined,
+    }, //have some cookie please
+    exchanges: [
+      dedupExchange,
+      cacheExchange({
+        keys: {
+          PaginatedPosts: () => null,
         },
-      },
-      updates: {
-        // updating query after our mutation fires
-        Mutation: {
-          logout: (_result: LogoutMutation, _args, cache, _info) => {
-            cache.updateQuery<MeQuery, QueryInput>(
-              { query: MeDocument },
-              () => {
-                return { me: null };
-              }
-            );
-          },
-          login: (result: LoginMutation, _args, cache, _info) => {
-            cache.updateQuery<MeQuery, QueryInput>(
-              { query: MeDocument },
-              (data) => {
-                // you can do immer like updates
-                if (result.login.errors) {
-                  return data; // same as the cache
-                } else {
-                  return {
-                    me: result.login.user, // login success then, update Me cache with the user
-                  };
-                }
-              }
-            );
-          },
-          register: (result: RegisterMutation, _args, cache, _info) => {
-            cache.updateQuery<MeQuery, QueryInput>(
-              { query: MeDocument },
-              (data) => {
-                // you can do immer like updates
-                if (result.register.errors) {
-                  return data;
-                } else {
-                  return {
-                    me: result.register.user,
-                  };
-                }
-              }
-            );
+        resolvers: {
+          Query: {
+            posts: cursorPagination(),
           },
         },
-      },
-    }),
-    errorExchange,
-    ssrExchange,
-    fetchExchange,
-  ],
-});
+        updates: {
+          // updating query after our mutation fires
+          Mutation: {
+            vote: (
+              _result: VoteMutation,
+              args: VoteMutationVariables,
+              cache,
+              _info
+            ) => {
+              const data = cache.readFragment(
+                gql`
+                  fragment _ on Post {
+                    id
+                    points
+                    voteStatus
+                  }
+                `,
+                { id: args.postId }
+              );
+              if (args.value === data.voteStatus) {
+                return;
+              }
+              const newPoints: number =
+                data.points + args.value * (!data.voteStatus ? 1 : 2); // add the value, if newly voted then add or subtract 1, if voted before the 2
+              cache.writeFragment(
+                gql`
+                  fragment _ on Post {
+                    points
+                    voteStatus
+                  }
+                `,
+                {
+                  id: data.id,
+                  points: newPoints,
+                  voteStatus: args.value,
+                }
+              );
+            },
+            logout: (_result: LogoutMutation, _args, cache, _info) => {
+              cache.updateQuery<MeQuery, QueryInput>(
+                { query: MeDocument },
+                () => {
+                  return { me: null };
+                }
+              );
+            },
+            login: (result: LoginMutation, _args, cache, _info) => {
+              cache.updateQuery<MeQuery, QueryInput>(
+                { query: MeDocument },
+                (data) => {
+                  // you can do immer like updates
+                  if (result.login.errors) {
+                    return data; // same as the cache
+                  } else {
+                    return {
+                      me: result.login.user, // login success then, update Me cache with the user
+                    };
+                  }
+                }
+              );
+            },
+            register: (result: RegisterMutation, _args, cache, _info) => {
+              cache.updateQuery<MeQuery, QueryInput>(
+                { query: MeDocument },
+                (data) => {
+                  // you can do immer like updates
+                  if (result.register.errors) {
+                    return data;
+                  } else {
+                    return {
+                      me: result.register.user,
+                    };
+                  }
+                }
+              );
+            },
+          },
+        },
+      }),
+      errorExchange,
+      ssrExchange,
+      fetchExchange,
+    ],
+  };
+};
