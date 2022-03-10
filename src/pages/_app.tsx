77683d8892183916801238ba62b0616f1d0be637
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2022-03-10 23:32:26 +0600

    apollo cache provider for whole app, merge posts of different request

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,34 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 import theme from '../theme';
+import { PaginatedPosts } from '../generated/graphql';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  credentials: 'include',
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          posts: {
+            keyArgs: false, // or [], don't save posts different place based on {limit:15, curso:'1345462243224'} etc. keyArgs
+            merge(
+              existing: PaginatedPosts,
+              incoming: PaginatedPosts
+            ): PaginatedPosts {
+              return {
+                ...incoming,
+                posts: [...(existing?.posts || []), ...incoming.posts],
+              };
+            },
+          },
+        },
+      },
+    },
+  }),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -11,7 +38,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           useSystemColorMode: true,
         }}
       >
-        <Component {...pageProps} />
+        <ApolloProvider client={client}>
+          <Component {...pageProps} />
+        </ApolloProvider>
       </ColorModeProvider>
     </ChakraProvider>
   );
